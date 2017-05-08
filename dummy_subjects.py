ea4f6c279251269268886b6cdc6024c7c24f7739
Alphy Joseph <lizjoseph18@rocketmail.com>
2017-05-08 22:59:48 +0530

    subjects db population

--- dummy_subjects.py
+++ dummy_subjects.py
@@ -14,11 +14,17 @@ session = Session()
 
 
 #######################	One dummy user
-semes = Semester(2,'Physics,Electrical,Civil')
+semes = Semester(1,'Engineering Mathematics-1,Physics,Civil, Mechanical, Electrical,')
 session.add(semes)
-semes = Semester(3,'adas,asda,Civil')
+semes = Semester(2,'Engineering Mathematics-2,Chemistry,Programming in C,Electronics')
 session.add(semes)
-semes = Semester(4,'dfgghn,asd,Civil')
+semes = Semester(3,'Engineering Mathematics-3,Electronic Circuits,Logic Design, Discrete Mathematical Structures,Data Structures with C, OOP with C++')
+session.add(semes)
+semes = Semester(4,'Engineering Mathematics-4, Graph Theory, ADA, Unix and Shell Programming, Microprocessors, Computer Organization')
+session.add(semes)
+semes = Semester(5,'Software Engineering, System Software, Operating Systems, DBMS, FLAT,Computer Networks-1')
+session.add(semes)
+semes = Semester(6,'Management & Entrepreneurship,Unix System Programming,Compiler Design, Computer Networks-2,Computer Graphics, Operations Research,Pattern Recognition')
 session.add(semes)
 #####################################33
  
